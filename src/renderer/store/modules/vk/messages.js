afc896296191f35edc75492c23a7eb880062b22c
Maxim <purplehorrorrus@gmail.com>
2022-07-04 20:40:28 +0800

    Fix new adding messages to cache

--- src/renderer/store/modules/vk/messages.js
+++ src/renderer/store/modules/vk/messages.js
@@ -73,29 +73,29 @@ export default {
         },
 
         ADD_MESSAGE: async ({ state, rootState }, data) => {
-            const id = data.payload.message.from_id;
+            const peer_id = data.payload.message.peer_id;
 
-            data.payload.message.out = data.payload.message.out 
-                || data.payload.message.from_id === rootState.vk.user.id;
+            data.payload.message.out = Number(data.payload.message.out 
+                || data.payload.message.from_id === rootState.vk.user.id);
 
-            if (id in state.cache) {
+            if (peer_id in state.cache) {
                 const response = await rootState.vk.client.api.messages.getById({
                     message_ids: data.payload.message.id
                 });
 
-                state.cache[id].count++;
+                state.cache[peer_id].count++;
                 if (data.payload.message.out) {
-                    const messageIndex = state.cache[id].messages.findIndex(message => {
+                    const messageIndex = state.cache[peer_id].messages.findIndex(message => {
                         return message.random_id === data.payload.message.random_id;
                     });
 
                     if (~messageIndex) {
-                        state.cache[id].messages[messageIndex].id = response.items[0].id;
-                        state.cache[id].messages[messageIndex] = response.items[0];
-                    } else state.cache[id].messages.push(response.items[0]);
-                } else state.cache[id].messages.push(response.items[0]);
+                        state.cache[peer_id].messages[messageIndex].id = response.items[0].id;
+                        state.cache[peer_id].messages[messageIndex] = response.items[0];
+                    } else state.cache[peer_id].messages.push(response.items[0]);
+                } else state.cache[peer_id].messages.push(response.items[0]);
 
-                return state.cache[id];
+                return state.cache[peer_id];
             }
 
             return false;
