6ea7019c9a8709220f0797647892a4f01eae1666
Maxim <purplehorrorrus@gmail.com>
2022-07-25 16:35:35 +0800

    Update version

--- src/main/common.js
+++ src/main/common.js
@@ -7,7 +7,7 @@ import { app } from "electron";
 import storage from "./storage";
 import WindowsLogic from "./windows/logic";
 
-app.getVersion = () => "1.0.0-beta.1";
+app.getVersion = () => "1.0.0-beta.2";
 
 const isDev = process.env.NODE_ENV === "development";
 const isWindows = process.platform === "win32";
